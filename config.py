02cd6fd33a470c5c2f58a49bcd1494a92b707916
pedronovaes <pedronovaesmelo@gmail.com>
2021-03-21 12:49:49 -0300

    First version of database with User and Post tables

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = '5c2ab8adf5a6b725eb876f2c3638140c'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///socialco.db'

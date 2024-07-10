e7f5a7287a519870e8f291be5bcebf9fc223423a
pedronovaes <pedronovaesmelo@gmail.com>
2024-07-09 22:32:38 -0300

    Using a mock user and post to render a first version of index page

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
 # The routes module handle the different URLs that the application supports.
 from app import app
+from flask import render_template
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, world!'
+    user = {'username': 'Pedro'}
+    posts = [
+        {'author': {'username': 'Pedro'}, 'body': 'Lindo dia em Monte Verde!'},
+        {'author': {'username': 'Carol'}, 'body': 'O Corinthians é o mundo!'},
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)

8a47e92e970db3d8a94f191917d8daafce8a67de
pedronovaes <pedronovaesmelo@gmail.com>
2025-02-09 17:46:25 -0300

    Improve file structure by using templates

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,30 @@
 """
-This module handle the different URLs that the application supports using view
-functions.
+This module handles several routes that social.co supports using view fucntions.
 """
 
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, social.co!'
+    user = {'username': 'Pedro'}
+    posts = [
+        {
+            'author': {'username': 'Carol'},
+            'body': 'Lindo dia em João Dourado!'
+        },
+        {
+            'author': {'username': 'Hamilton'},
+            'body': 'Pilotarei pela Ferrari em 2025 :)'
+        }
+    ]
+
+    # Invokes Jinja template engine to render index.html page.
+    return render_template(
+        template_name_or_list='index.html',
+        title='Home',
+        user=user,
+        posts=posts
+    )

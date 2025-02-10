86950cb99fc04699163aa13ec6f400823be35203
pedronovaes <pedronovaesmelo@gmail.com>
2025-02-09 23:51:36 -0300

    Initial login structure with login's form, template and views

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 """
-This module handles several routes that social.co supports using view fucntions.
+This module handles several routes that social.co supports using view
+functions.
 """
 
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -28,3 +30,19 @@ def index():
         user=user,
         posts=posts
     )
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """View function to login a user."""
+
+    form = LoginForm()
+
+    # All form processing work (receiving login credentials and so on).
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+
+        return redirect(location='/index')
+
+    # Executes when the browser sends the GET request to receive the web page
+    # with the form.
+    return render_template('login.html', title='Sign In', form=form)

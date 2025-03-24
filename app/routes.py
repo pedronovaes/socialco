701dca7b5dc2bdf66ab6115357ea88e6c76fa6ec
pedronovaes <pedronovaesmelo@gmail.com>
2025-03-24 00:43:01 -0300

    First structure of user profile page

--- app/routes.py
+++ app/routes.py
@@ -110,3 +110,16 @@ def register():
         title='Register',
         form=form
     )
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+
+    return render_template('user.html', user=user, posts=posts)

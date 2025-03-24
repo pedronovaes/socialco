01b0f5d8a87b927c733f9115528092c0ce75d8aa
pedronovaes <pedronovaesmelo@gmail.com>
2025-03-24 19:20:57 -0300

    Add profile editor codes

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,20 @@
 """Module to store web form classes."""
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms import (
+    StringField,
+    PasswordField,
+    BooleanField,
+    SubmitField,
+    TextAreaField
+)
+from wtforms.validators import (
+    DataRequired,
+    ValidationError,
+    Email,
+    EqualTo,
+    Length
+)
 
 import sqlalchemy as sa
 from app import db
@@ -45,3 +57,9 @@ class RegistrationForm(FlaskForm):
 
         if user is not None:
             raise ValidationError('Please use a different email address.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField(label='Username', validators=[DataRequired()])
+    about_me = TextAreaField(label='About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')

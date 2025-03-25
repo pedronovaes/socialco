fc0f1ca6a8d8270cf3f633ba244c38015b6b6a5b
pedronovaes <pedronovaesmelo@gmail.com>
2025-03-25 10:38:11 -0300

    Module to send errors by email

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
+import logging
+from logging.handlers import SMTPHandler
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+
 from config import Config
 
 app = Flask(__name__)
@@ -12,5 +16,30 @@ migrate = Migrate(app=app, db=db)  # Database migration engine.
 
 login = LoginManager(app=app)  # Login initialization.
 login.login_view = 'login'  # Forces users to log in before view certain pages.
+
+# Create error handler.
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        auth = None
+
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+
+        secure = None
+
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            toaddrs=app.config['ADMINS'],
+            subject='Social.co Failure',
+            credentials=auth,
+            secure=secure
+        )
+
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(hdlr=mail_handler)
 
 from app import routes, models, errors
--- config.py
+++ config.py
@@ -15,3 +15,11 @@ class Config:
         or
         'sqlite:///' + os.path.join(basedir, 'app.db')
     )
+
+    # Email to handling errors.
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None  # Enxrypted conn
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['pedronovaesmelo@gmail.com']
